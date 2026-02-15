d415bd3143570e569fbd189890b40e8aacf3dfa1
Your Name <you@example.com>
2026-02-15 01:24:16 -0300

    feat:adicao do metodo de categoria

---
+++ src/main/java/projeto_base_de_telas_e_login/adapter/in/web/controllers/TelaProdutos/TelaCategoria.java
@@ -0,0 +1,4 @@
+package projeto_base_de_telas_e_login.adapter.in.web.controllers.TelaProdutos;
+
+public class TelaCategoria {
+}
---
+++ src/main/java/projeto_base_de_telas_e_login/domain/UseCase/Categoria/CategoriaUseCase.java
@@ -0,0 +1,4 @@
+package projeto_base_de_telas_e_login.domain.UseCase.Categoria;
+
+public class CategoriaUseCase {
+}
